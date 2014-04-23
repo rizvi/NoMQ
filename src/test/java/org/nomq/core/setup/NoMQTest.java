0e114deab70504e2e00d499c20da44d632b5fd3e
Tommy Wassgren <tommy.wassgren@gmail.com>
2014-04-23 22:32:42 +0200

    Added test case for setup class.

--- src/test/java/org/nomq/core/setup/NoMQTest.java
+++ src/test/java/org/nomq/core/setup/NoMQTest.java
@@ -16,18 +16,60 @@
 
 package org.nomq.core.setup;
 
+import com.hazelcast.core.Hazelcast;
 import org.junit.Test;
 import org.nomq.core.Event;
+import org.nomq.core.process.JournalEventStore;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.junit.Assert.assertEquals;
 
 public class NoMQTest {
+    @Test
+    public void testAdvancedSetup() throws InterruptedException, IOException {
+        // Given
+        final Path recordFolder = Files.createTempDirectory("org.nomq.test");
+        final JournalEventStore recordEventStore = new JournalEventStore(recordFolder.toString());
+
+        final Path playFolder = Files.createTempDirectory("org.nomq.test");
+        final JournalEventStore playEventStore = new JournalEventStore(playFolder.toString());
+
+        final ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        final List<Event> result = new ArrayList<>();
+        final NoMQ noMQ =
+                new NoMQ.Builder()
+                        .play(playEventStore)
+                        .record(recordEventStore)
+                        .playQueue(new LinkedBlockingQueue<>())
+                        .executorService(executorService)
+                        .topic("testTopic")
+                        .eventSubscribers(result::add)
+                        .hazelcast(Hazelcast.newHazelcastInstance())
+                        .build();
+        noMQ.start();
+
+        // When
+        noMQ.publisher().publish("Simple event".getBytes());
+
+        // Wait for the message to be delivered
+        Thread.sleep(200);
+
+        // Then
+        assertEquals(1, result.size());
+
+        // Cleanup
+        noMQ.stop();
+    }
+
     @Test
     public void testSetupWithFolders() throws InterruptedException, IOException {
         // Given
