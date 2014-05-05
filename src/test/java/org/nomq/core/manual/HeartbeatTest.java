5896bc23a5df9847da6e46dbcab82728e691eda3
Tommy Wassgren <tommy.wassgren@gmail.com>
2014-05-05 21:21:32 +0200

    Updated with simple manual test used for heartbeating

---
+++ src/test/java/org/nomq/core/manual/HeartbeatTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.nomq.core.manual;
+
+import org.nomq.core.NoMQ;
+import org.nomq.core.NoMQBuilder;
+import org.nomq.core.store.JournalEventStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author Tommy Wassgren
+ */
+public class HeartbeatTest {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    public static void main(final String[] args) throws IOException {
+        new HeartbeatTest();
+    }
+
+    private HeartbeatTest() throws IOException {
+        final JournalEventStore recordEventStore = new JournalEventStore(tempFolder());
+        final ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(10);
+        final NoMQ noMQ = NoMQBuilder
+                .builder()
+                .record(recordEventStore)
+                .executorService(scheduledExecutorService)
+                .subscribe(e -> log.info("Ping [id={}, count={}]", e.id(), new String(e.payload())))
+                .build()
+                .start();
+
+        final AtomicInteger integer = new AtomicInteger(0);
+        scheduledExecutorService.scheduleWithFixedDelay(
+                () -> noMQ.publishAsync(
+                        "ping",
+                        integer.incrementAndGet(),
+                        i -> Integer.toString(i).getBytes()),
+                5000,
+                5000,
+                TimeUnit.MILLISECONDS
+        );
+    }
+
+    private String tempFolder() throws IOException {
+        return Files.createTempDirectory("org.nomq.test").toString();
+    }
+}
